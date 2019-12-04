af37ca0757ef1a5f6bc33024394b778967d6cc35
raphaelgyo <raphael.granizo@gmail.com>
2019-12-04 18:50:45 +0100

    1st commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 4 Exercice 2 : Les fonctions</title>
+  </head>
+  <body>
+    <?php
+    function DireBonjour($nom){
+      echo 'Bonjour ' . $nom . '!';
+    }
+    DireBonjour('Père Noël');
+    ?>
+  </body>
+</html>
